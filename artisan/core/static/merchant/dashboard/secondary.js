eeb2d1cc2682a60a8e1bac76715a031207e0f70a
LukeGeyer1117 <luke.geyer@utahtech.edu>
2025-07-02 16:59:33 -0600

    More dashboard implementation

--- artisan/core/static/merchant/dashboard/secondary.js
+++ artisan/core/static/merchant/dashboard/secondary.js
@@ -1,15 +1,37 @@
+let products = []
+const API_BASE_URL = `${window.location.protocol}//${window.location.hostname}:8000/api`;
+let artisan;
+
 document.addEventListener('DOMContentLoaded', async function () {
   // Get the Artisan information at page load
-    await fetch(`${API_BASE_URL}/artisan`, {
-        method: 'GET',
-        credentials: 'include',
-    })
-    .then(response => {
-        if (!response.ok) throw new Error("Could Not Get Artisan Info!");
-        return response.json();
-    })
-    .then(data => {
-        artisan = data.artisan;
-        console.log(artisan)
-    })
+  await fetch(`${API_BASE_URL}/artisan`, {
+      method: 'GET',
+      credentials: 'include',
+  })
+  .then(response => {
+      if (!response.ok) throw new Error("Could Not Get Artisan Info!");
+      return response.json();
+  })
+  .then(data => {
+      artisan = data.artisan;
+      console.log(artisan)
+  })
+
+  // Handle Signout button click
+  const signOutBtn = document.querySelector('.navbar a');
+  signOutBtn.addEventListener('click', (event) => {
+      fetch(`${API_BASE_URL}/session/`, {
+          method: 'DELETE'
+      })
+      .then(response => {
+          if (!response.ok) throw new Error('Failed to clear session data.');
+          return response.json();
+      })
+      .then(result => {
+          window.location.href = "/login/"
+      })
+  })
+
+  // Get all orders assigned to an artisan
+  
 })
