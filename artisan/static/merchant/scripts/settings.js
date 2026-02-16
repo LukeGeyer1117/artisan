f4635b0ad6a17ea3043f7c71ac90f06e53748378
LukeGeyer1117 <luke.geyer@utahtech.edu>
2026-02-16 10:08:54 -0700

    updated settings to store last accessed tab in localstorage, also added a splash page at the / route

--- artisan/static/merchant/scripts/settings.js
+++ artisan/static/merchant/scripts/settings.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const initialTab = document.querySelector('.tab');
-  initialTab.classList.add('tab-active');
-  initialTab.classList.add('text-primary');
-
-
   const tabs = document.querySelectorAll('.tab');
+  const cached_tab = localStorage.getItem('tab');
+  if (cached_tab) {
+    tabs.forEach(tab => {
+      if (tab.dataset.tab === cached_tab) {
+        tab.classList.add('tab-active');
+        tab.classList.add('tab-primary');
+      }
+    })
+  } else {
+    const initialTab = document.querySelector('.tab');
+    initialTab.classList.add('tab-active');
+    initialTab.classList.add('text-primary');
+  }
+
   tabs.forEach(tab => {
     tab.addEventListener('click', function () {
       tabs.forEach(tab => {
@@ -13,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
       })
       tab.classList.add('tab-active');
       tab.classList.add('text-primary');
+      localStorage.setItem('tab', tab.dataset.tab);
     })
   })
 })
