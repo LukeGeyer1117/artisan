de3f77875d93a16ca0cfb46bc801447612b842bb
LukeGeyer1117 <luke.geyer@utahtech.edu>
2026-02-18 09:33:17 -0700

    added multiple default auth methods

--- artisan/artisan/settings/base.py
+++ artisan/artisan/settings/base.py
@@ -109,6 +109,15 @@ SIMPLE_JWT = {
   "ALGORITHM": "HS256",
 }
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+
 # Internationalization
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
